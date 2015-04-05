8b2e2ef96af168c50c37595245612558c2af7096
Dylan Gijsbertsen <d.b.gijsbertsen@live.nl>
2015-04-05 18:35:34 +0200

    put some comments for the player script and changed the player script a little for walking. Created new animation for the bomb minion and a simple AI script

--- BulletBoy Unity/Assets/Scripts/BombMinionScript.cs
+++ BulletBoy Unity/Assets/Scripts/BombMinionScript.cs
@@ -4,26 +4,41 @@ using System.Collections;
 public class BombMinionScript : MonoBehaviour {
 
     public float _speed = 3;
+    Rigidbody2D rb;
+    private bool walkingRight = true;
 
+    public Transform objectCheck;
+    private bool hit;
+    private float radius = .01f;
+    public LayerMask whatIsHit;
+
+    Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        hit = Physics2D.OverlapCircle(objectCheck.position, radius, whatIsHit);
+        if (hit) { walkingRight = !walkingRight; }
         EnemyBehaviour();
 	}
 
     void EnemyBehaviour()
     {
-        transform.Translate(Vector2.right * _speed * Time.deltaTime);
-        if (_speed < 0)
+        
+        if (walkingRight)
         {
+            anim.SetTrigger("GoWalk");
+            transform.Translate(-Vector2.right * _speed * Time.deltaTime);
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if (_speed > 0)
+        else
         {
+            anim.SetTrigger("GoWalk");
+            transform.Translate(Vector2.right * _speed * Time.deltaTime);
             transform.localScale = new Vector3(1, 1, 1);
         }
     }
--- BulletBoy Unity/Assets/Scripts/PlayerEngine.cs
+++ BulletBoy Unity/Assets/Scripts/PlayerEngine.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerEngine : MonoBehaviour {
+public class PlayerEngine : MonoBehaviour
+{
 
-    private float _speed = .5f, _maxSpeed = 21;
-    public string walkInput;
+    private float _speed = 10f; // walking speed
+    public string walkInput; // Horizontal
     Rigidbody2D rb;
 
     [Header("Jumping")]
@@ -12,8 +13,8 @@ public class PlayerEngine : MonoBehaviour {
     public Transform groundCheck;
     private float radius = 0.2f;
     public bool grounded = false;
-    public LayerMask whatIsGround;
-    private int _jumps = 0, _maxJumps = 2;
+    public LayerMask whatIsGround; // all layers of what is ground 
+    private int _jumps = 0, _maxJumps = 2; // maximum of jumps
 
     Animator anim;
 
@@ -23,46 +24,44 @@ public class PlayerEngine : MonoBehaviour {
         anim = GetComponent<Animator>();
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         grounded = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsGround); // Check if it is on ground
         anim.SetBool("Ground", grounded);
         anim.SetFloat("vSpeed", rb.velocity.y); // checking vertical speed
 
         if (grounded) { _jumps = 0; }
-            
+
         Movement();
-	}
+    }
 
     /// <summary>
     /// The player moves and jumps around
     /// </summary>
     private void Movement()
     {
-        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis(walkInput)));
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis(walkInput))); // when there is input play anitmation
         if (Input.GetAxis(walkInput) < -.1f) // !! Also have to put in the button for controller movement!!
         {
-            if (rb.velocity.x > -_maxSpeed)
-            {
-                transform.localScale = new Vector3(-1, 1, 1);
-                rb.velocity = new Vector2(-_speed * _maxSpeed, rb.velocity.y);
-            }
+            transform.localScale = new Vector3(-1, 1, 1);// change the sprite to the right direction
+            transform.Translate(-Vector2.right * _speed * Time.deltaTime); // move the player to the left
+
         }
         else if (Input.GetAxis(walkInput) > .1f)// !! also have to put in the button for controller movement !!
         {
-            if (rb.velocity.x < _maxSpeed)
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-                rb.velocity = new Vector2(_speed * _maxSpeed, rb.velocity.y);
-            }
+            transform.localScale = new Vector3(1, 1, 1); // change the sprite to the right direction
+            transform.Translate(Vector2.right * _speed * Time.deltaTime); // move the player to the right
+
         }
 
-        if (Input.GetButtonDown("Jump") && _jumps < _maxJumps)
+        if (Input.GetButtonDown("Jump") && _jumps < _maxJumps) // when press on the jump button activate Jump method
         {
             Jump();
         }
