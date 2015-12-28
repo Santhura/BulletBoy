e672a7bccb4f5100ad24c2e2ecdd18b179b0902a
Dylan Main PC <d.b.gijsbertsen@live.nl>
2015-12-28 23:46:07 +0100

    little bullet script

--- BulletBoy Unity/Assets/Resources/Scripts/Items/LittleBullet.cs
+++ BulletBoy Unity/Assets/Resources/Scripts/Items/LittleBullet.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class LittleBullet : MonoBehaviour {
 
+    Animator anim;
 
+    private float originalY;
+    public float floatStrength = 1;
 
 	// Use this for initialization
 	void Start () {
-	
+        anim = GetComponent<Animator>();
+        originalY = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.position = new Vector3(transform.position.x, originalY + ((float)Mathf.Sin(Time.time) * floatStrength), transform.position.z);
 	}
 }
