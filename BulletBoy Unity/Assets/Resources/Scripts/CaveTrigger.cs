45580db7ba0f1cad6e3689af8d92ad227950e06c
Dylan Main PC <d.b.gijsbertsen@live.nl>
2015-12-30 18:09:56 +0100

    cave transparent added

---
+++ BulletBoy Unity/Assets/Resources/Scripts/CaveTrigger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveTrigger : MonoBehaviour
+{
+
+
+    public SpriteRenderer alphaMountain;
+    public bool inCave = false;
+    
+    private float alphaColor = 1;
+    private float numberTrigger;
+    public CircleCollider2D playerCircleCol;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Physics2D.IgnoreCollision(this.GetComponent<BoxCollider2D>(), playerCircleCol);
+        if (inCave)
+        {
+            alphaColor -= 0.3f * Time.deltaTime;
+            if (alphaColor <= 0)
+            {
+                alphaColor = 0;
+            }
+        }
+        else
+        {
+            alphaColor = 1;
+        }
+        alphaMountain.color = new Color(1, 1, 1, alphaColor);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            if (numberTrigger == 0)
+            {
+                inCave = true;
+                numberTrigger = 1;
+            }
+            else if (numberTrigger >= 1)
+            {
+                inCave = false;
+                numberTrigger = 0;
+            }
+        }
+    }
+}
